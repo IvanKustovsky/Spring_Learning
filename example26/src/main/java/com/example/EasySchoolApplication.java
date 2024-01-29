ff6f44ec327c06e72e5f998d9373a442d6fd132b
Ivan <vanyakustovsky@gmail.com>
2024-01-29 13:42:02 +0200

    Securing Web Application with InMemory users & Custom Login Page

--- example26/src/main/java/com/example/EasySchoolApplication.java
+++ example26/src/main/java/com/example/EasySchoolApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EasySchoolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EasySchoolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EasySchoolApplication.class, args);
+    }
 
 }
--- example26/src/main/java/com/example/config/ProjectSecurityConfig.java
+++ example26/src/main/java/com/example/config/ProjectSecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,15 +18,36 @@ public class ProjectSecurityConfig {
 
         http.csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/dashboard").authenticated() // default behaviour
                         .requestMatchers("/home", "/").permitAll()
                         .requestMatchers("/holidays/**").permitAll()
                         .requestMatchers("/contact").permitAll()
                         .requestMatchers("/saveMsg").permitAll()
                         .requestMatchers("/courses").permitAll()
                         .requestMatchers("/about").permitAll()
-                        .requestMatchers("/assets/**").permitAll());
-        http.formLogin(Customizer.withDefaults());
+                        .requestMatchers("/assets/**").permitAll()
+                        .requestMatchers("/login").permitAll());
+        http.formLogin(loginConfigurer -> loginConfigurer.loginPage("/login")
+                .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll());
+        http.logout(logoutConfigurer -> logoutConfigurer.logoutSuccessUrl("/login?logout=true")
+                .invalidateHttpSession(true).permitAll());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() { // NOT recommended for production
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin")
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
