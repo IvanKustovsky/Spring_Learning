1eff679e2abd584442a8a2fe70a1241d268a7d18
Ivan <vanyakustovsky@gmail.com>
2024-02-19 15:48:33 +0200

    Added Content filter inside REST Services using JsonIgnore annotation

--- example32/src/main/java/com/example/model/BaseEntity.java
+++ example32/src/main/java/com/example/model/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.example.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -19,18 +20,22 @@ public class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false) // won't be updated when using "update" SQL operation
+    @JsonIgnore
     private LocalDateTime createdAt;
 
     @CreatedBy
     @Column(updatable = false)
+    @JsonIgnore // this field will NOT go inside my JSON response of my rest services.
     private String createdBy;
 
     @LastModifiedDate
     @Column(insertable = false) // won't be inserted when using "insert" SQL operation
+    @JsonIgnore
     private LocalDateTime updatedAt;
 
     @LastModifiedBy
     @Column(insertable = false)
+    @JsonIgnore
     private String updatedBy;
 
 }
