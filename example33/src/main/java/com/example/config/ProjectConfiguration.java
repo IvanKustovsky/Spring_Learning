26801f13e116c0d741c2c7235c664af391df79c8
Ivan <vanyakustovsky@gmail.com>
2024-02-20 14:26:18 +0200

    Consuming REST Services using RestTemplate

--- example33/src/main/java/com/example/config/ProjectConfiguration.java
+++ example33/src/main/java/com/example/config/ProjectConfiguration.java
@@ -1,8 +1,10 @@
 package com.example.config;
 
 import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ProjectConfiguration {
@@ -10,5 +12,12 @@ public class ProjectConfiguration {
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
         return new BasicAuthRequestInterceptor("admin@gmail.com", "admin");
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+        return restTemplateBuilder.basicAuthentication
+                        ("admin@gmail.com", "admin").build();
     }
 }
--- example33/src/main/java/com/example/controller/ContactController.java
+++ example33/src/main/java/com/example/controller/ContactController.java
@@ -1,12 +1,16 @@
 package com.example.controller;
 
 import com.example.model.Contact;
+import com.example.model.Response;
 import com.example.proxy.ContactProxy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -17,8 +21,24 @@ public class ContactController {
     @Autowired
     private ContactProxy contactProxy;
 
+    @Autowired
+    private RestTemplate restTemplate; // Is not recommended for using
+
     @GetMapping("/getMessages")
     public List<Contact> getMessages(@RequestParam("status") String status) {
         return contactProxy.findMessagesByStatus(status);
     }
+
+    @PostMapping("/saveMsg")
+    public ResponseEntity<Response> saveMsg(@RequestBody Contact contact){
+        String uri = "http://localhost:8080/api/contact/saveMsg"; // In Prod should be stored inside properties or constant file
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("invocationFrom","RestTemplate");
+        HttpEntity<Contact> httpEntity = new HttpEntity<>(contact, headers);
+        ResponseEntity<Response> responseEntity = restTemplate.exchange(uri, HttpMethod.POST,
+                httpEntity,Response.class);
+        return responseEntity;
+    }
+
+
 }
